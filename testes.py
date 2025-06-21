7f65a605e5acd94c44a5878a4dd00faeef642cb4
joaovdorio <joaovitor.dorio@gmail.com>
2025-06-21 12:46:54 -0300

    atualizando projeto

---
+++ testes.py
@@ -0,0 +1,34 @@
+import pytest
+import requests
+
+# CRUD
+BASE_URL = 'http://127.0.0.1:5000'
+tasks = []
+
+def test_createtask():
+    new_task_data = {
+        "title": "Nova tarefa",
+        "description":"descrição da nova tarefa"
+    }
+    
+    response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+    tasks.append(response_json['id'])
+
+def test_readtask():
+    response = requests.get(f"{BASE_URL}/tasks")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "tasks" in response_json
+    assert "total_tasks" in response_json
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']
